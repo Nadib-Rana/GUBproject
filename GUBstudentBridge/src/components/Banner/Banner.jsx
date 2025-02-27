927505078b6d9d96af4f8163bf944ca9c2a92cbe
nadib <codecrafersnadib@gmail.com>
2025-02-27 21:16:17 +0600

    working start

---
+++ GUBstudentBridge/src/components/Banner/Banner.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Banner.css';
+
+export const Banner = () => {
+  return (
+    <div className="food-banner"> 
+      <div className='food-banner-left'>
+        <h2 className='food-banner-titel'>
+          Savor the  favorite
+           </h2>
+        <p className='food-banner-description'>
+        Experience delicious dishes, fresh ingredients, and culinary delights.
+        </p>
+      </div>
+      <div className='food-banner-right'>
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
+};
