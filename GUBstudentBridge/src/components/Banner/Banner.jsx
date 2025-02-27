be823b9c2fb44508f560820d101fd3a35fc91b71
nadib <codecrafersnadib@gmail.com>
2025-02-27 22:11:00 +0600

    ..

--- GUBstudentBridge/src/components/Banner/Banner.jsx
+++ GUBstudentBridge/src/components/Banner/Banner.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import './Banner.css';
+import lunchFood from '../../assets/images/LunchFoodCanteen.jpeg';
+import biriyani from '../../assets/images/biriyani.jpeg';
+import breakfastFood from '../../assets/images/backfirstFoodCanteen.jpeg';
 
 export const Banner = () => {
   return (
-    <div className="food-banner"> 
+    <div className="food-banner">
       <div className='food-banner-left'>
         <h2 className='food-banner-titel'>
-          Savor the  favorite
-           </h2>
+          Savor the favorite
+        </h2>
         <p className='food-banner-description'>
-        Experience delicious dishes, fresh ingredients, and culinary delights.
+          Experience delicious dishes, fresh ingredients, and culinary delights.
         </p>
       </div>
       <div className='food-banner-right'>
-        <img src="" alt="" />
+        <img src={lunchFood} alt="Lunch Food Canteen" />
+        <img src={biriyani} alt="Biriyani" />
+        <img src={breakfastFood} alt="Breakfast Food Canteen" />
       </div>
     </div>
   );
