69dc49625c324f95e9855015a3ba6418545e9a1f
nadib <codecrafersnadib@gmail.com>
2025-03-01 02:00:50 +0600

    banner

--- GUBstudentBridge/src/components/Banner/Banner.jsx
+++ GUBstudentBridge/src/components/Banner/Banner.jsx
@@ -7,15 +7,16 @@ import breakfastFood from '../../assets/images/backfirstFoodCanteen.jpeg';
 export const Banner = () => {
   return (
     <div className="food-banner">
-      <div className='food-banner-left'>
-        <h2 className='food-banner-titel'>
+      <div className="food-banner-left">
+        <h2 className="food-banner-title">
           Savor the favorite
         </h2>
-        <p className='food-banner-description'>
+        <p className="food-banner-description">
           Experience delicious dishes, fresh ingredients, and culinary delights.
         </p>
+        <button className="cta-button">Order Now</button>
       </div>
-      <div className='food-banner-right'>
+      <div className="food-banner-right">
         <img src={lunchFood} alt="Lunch Food Canteen" />
         <img src={biriyani} alt="Biriyani" />
         <img src={breakfastFood} alt="Breakfast Food Canteen" />
